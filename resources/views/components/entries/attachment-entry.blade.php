f1e27475f011d6a266b141a7f5cf81b417cc33c4
Jyrki De Neve <jyrki.deneve@gmail.com>
2026-03-23 13:01:24 +0200

    Check if attachment in entry exists before trying to show it

--- resources/views/components/entries/attachment-entry.blade.php
+++ resources/views/components/entries/attachment-entry.blade.php
@@ -3,10 +3,12 @@
 @endphp
 
 <x-dynamic-component :component="$getEntryWrapperView()" :entry="$entry">
-    <x-filament-media-library::attachment
-        :$attachment
-        :show-title="false"
-        :show-tooltip="false"
-        container-class="h-80"
-    />
+    @if ($attachment)
+        <x-filament-media-library::attachment
+            :$attachment
+            :show-title="false"
+            :show-tooltip="false"
+            container-class="h-80"
+        />
+    @endif
 </x-dynamic-component>
