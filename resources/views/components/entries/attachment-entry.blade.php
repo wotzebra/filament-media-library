8127ad8dc6954aaabec5bc0d65478787e0f26109
DevolderLouise <louise.devolder@onlyhumans.com>
2026-04-01 14:42:47 +0200

    change height of preview image

--- resources/views/components/entries/attachment-entry.blade.php
+++ resources/views/components/entries/attachment-entry.blade.php
@@ -4,11 +4,13 @@
 
 <x-dynamic-component :component="$getEntryWrapperView()" :entry="$entry">
     @if ($attachment)
-        <x-filament-media-library::attachment
-            :$attachment
-            :show-title="false"
-            :show-tooltip="false"
-            container-class="h-80"
-        />
+        <div class="h-80 aspect-square">
+            <x-filament-media-library::attachment
+                :$attachment
+                :show-title="false"
+                :show-tooltip="false"
+                container-class="w-80"
+            />
+        </div>
     @endif
 </x-dynamic-component>
