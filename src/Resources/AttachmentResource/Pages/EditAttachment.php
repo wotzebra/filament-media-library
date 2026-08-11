181f87800e93e5aba117db7d5024f642b9ca986e
Jyrki De Neve <jyrki.deneve@gmail.com>
2026-08-11 11:28:09 +0300

    FIL001-104 Upgrade to new major version of TranslatableTabs package

--- src/Resources/AttachmentResource/Pages/EditAttachment.php
+++ src/Resources/AttachmentResource/Pages/EditAttachment.php
@@ -6,11 +6,9 @@ use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
 use Wotz\MediaLibrary\Resources\AttachmentResource;
 use Wotz\MediaLibrary\Resources\Concerns\HasVersionHistory;
-use Wotz\TranslatableTabs\Resources\Traits\HasTranslations;
 
 class EditAttachment extends EditRecord
 {
-    use HasTranslations;
     use HasVersionHistory;
 
     protected static string $resource = AttachmentResource::class;
